044713e229e7d8a8f59e824e357b6c987709ab55
Mrinmoy Haloi <69071143+MrinmoyHaloi@users.noreply.github.com>
2022-06-28 17:26:48 +0530

    changed more download options text

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -21,7 +21,7 @@
         <div class="col-12 text-center mt-3">
             <DownloadBtn />
             <div class="mt-2">
-                <a href="/downloads">other downloads</a> &nbsp&nbsp
+                <a href="/downloads">More options</a> &nbsp&nbsp
                 <a href="/docs/getting-started">get started</a>
             </div>
         </div>
